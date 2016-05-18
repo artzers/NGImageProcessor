94c3447c4fdc3a39195501747deedc4c40442e8d
artzers <263699527@qq.com>
2016-05-18 22:33:05 +0800

    add inverse filter

---
+++ BaseImageRestore.py
@@ -0,0 +1,44 @@
+import cv2, numpy as np
+import ImageNoiseGenerator
+
+class BaseImageRestore:
+    def __init__(self):
+        self.noiseGenerator = ImageNoiseGenerator.ImageNoiseGenerator()
+
+
+    def GenerateHDemo(self, sz):
+        u = sz[0]
+        v = sz[1]
+        H = np.zeros(sz, np.float64)
+        k=0.0025
+        for i in xrange(0, u):
+            for j in xrange(0, v):
+                H[i, j] = np.exp(-k * ( (i-u/2.0) ** 2.0 + (j-v/2.0) ** 2.0 ) ** (5.0/6.0))
+        return H
+
+    def IdeaLowHInverse(self, H, radius):
+        [u,v] = H.shape
+        for i in xrange(0, u):
+            for j in xrange(0, v):
+                if np.sqrt((i-u/2.0) ** 2.0 + (j - v / 2.0) ** 2.0) > radius or H[i, j] == 0:
+                    H[i, j] = 0
+                else:
+                    H[i, j] =  1.0 / H[i, j]
+        return H
+
+    def InverseFilterDemo(self, img):
+        #img = np.uint8(np.random.random((100, 100)) * 255)
+        H = self.GenerateHDemo(img.shape)
+        #H = np.float64(H)
+        fImg = np.fft.fftshift(np.fft.fft2(img))
+        gImg = np.fft.ifft2(np.fft.ifftshift(fImg*H))#(fImg.dot(H))
+        fgImg = np.fft.fftshift(np.fft.fft2(gImg))
+        H1 = self.IdeaLowHInverse(H, 200)
+        ggImg = np.fft.ifft2(np.fft.ifftshift(fgImg*H1))
+        cv2.namedWindow("g")
+        cv2.imshow("g", np.uint8(gImg))
+        cv2.namedWindow("inverse filter restore")
+        cv2.imshow("inverse filter restore", np.uint8(ggImg))
+        cv2.waitKey(0)
+
+
--- main.py
+++ main.py
@@ -6,27 +6,31 @@ import BasePixelTransfer
 import BaseSpatialFilter
 import BaseFFTProcessor
 import ImageNoiseGenerator
+import BaseImageRestore
 
-#baseTransfer = BasePixelTransfer.BasePixelTransfer()
-#img = cv2.imread("lena.jpg")
+baseTransfer = BasePixelTransfer.BasePixelTransfer()
+img = cv2.imread("lena.jpg")
 # #baseTransfer.HistDemo()
-#img = baseTransfer.RGB2Gray(img)
+img = baseTransfer.RGB2Gray(img)
 # baseSpacer = BaseSpatialFilter.BaseSpatialFilter()
 # baseSpacer.LaplacianDemo(img)
 #baseSpacer.LoGDemo(img, 5, 1)
 #baseSpacer.DoGDemo(img, 5, 0.3, 0.4)
 #baseSpacer.DoGCornerDetectDemo(img, 5, (0.3, 0.4, 0.6, 0.7, 0.7, 0.8), 2)
 
-ffter = BaseFFTProcessor.BaseFFTProcessor()
+#ffter = BaseFFTProcessor.BaseFFTProcessor()
 #ffter.FFTDemo()
 # ffter.IdeaLowPassDemo(50)
 #ffter.IdeaHighPassDemo(50)
 #ffter.GLPFDemo(50)
 # ffter.GHPFDemo(50)
 #ffter.LaplaceFFTDemo()
-ffter.LaplaceFFTDemo2()
+#ffter.LaplaceFFTDemo2()
 #noiser = ImageNoiseGenerator.ImageNoiseGenerator()
-#noiser.GuassNoise(img, 20, 10, 5000)
+#noiser.GuassNoise(img, 100, 10, 5000)
 #img = img[0:512, 0:512]
+imageRestore = BaseImageRestore.BaseImageRestore()
+imageRestore.InverseFilterDemo(img)
+
 
 
